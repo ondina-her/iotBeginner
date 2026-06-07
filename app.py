c3b1a26b44ce7ccdb0b44bd80c52382979ef1e78
ondina-her <20010878@galileo.edu>
2026-06-07 16:29:52 -0600

    sensor light

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
+import time
+
 from counterfit_connection import CounterFitConnection
+from counterfit_shims_grove.grove_light_sensor_v1_2 import GroveLightSensor
+
 CounterFitConnection.init('127.0.0.1', 5050)
+
+light_sensor = GroveLightSensor()
+
+while True:
+    light_value = light_sensor.get_light()
+    print(f"Light value: {light_value}")
+    CounterFitConnection.send_data(light_value)
+    time.sleep(1)
